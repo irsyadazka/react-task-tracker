efc3d68e3d815305cf8a9d0553b6c393a12a6abf
irsyadazka <16520096@mahasiswa.itb.ac.id>
2022-03-22 14:11:36 +0700

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom'
+import { useLocation } from "react-router-dom"
 
 const Footer = () => {
-    return (
+    const location = useLocation()
+    return (  
         <footer>
-            <p>Copyright &copy; 2022</p>
-            <Link to="/about">About</Link>
+            {location.pathname === '/about' ? <p>Copyright &copy; 2022</p> : 
+            <>
+                <p>Copyright &copy; 2022</p>
+                <Link to="/about">About</Link>
+            </>
+            }
         </footer>
+        
     )
 }
 
